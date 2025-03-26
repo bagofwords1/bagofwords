ceffeba1b1b7269fb430505ca0049d8b053d5896
yo <yochze@gmail.com>
2025-03-26 07:12:11 +0200

    tests

--- backend/alembic/versions/21952d7521e4_users_type.py
+++ backend/alembic/versions/21952d7521e4_users_type.py
@@ -35,7 +35,7 @@ def upgrade() -> None:
     sa.PrimaryKeyConstraint('id')
     )
     op.create_index(op.f('ix_user_email'), 'users', ['email'], unique=True)
-    op.create_index(op.f('ix_user_name'), 'users', ['name'], unique=True)
+    op.create_index(op.f('ix_user_name'), 'users', ['name'], unique=False)
     # ### end Alembic commands ###
 
 
