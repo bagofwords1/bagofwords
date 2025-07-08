d195703f6f260528803fde6e29580cc1c676de6f
yo <yochze@gmail.com>
2025-07-08 10:37:39 +0300

    slack

---
+++ frontend/pages/settings/integrations.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="mt-6">
+        <h1>Integrations</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { organization } = useOrganization()
+definePageMeta({
+    layout: 'settings'
+})
+</script>
